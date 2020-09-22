ccdf663a58eac49a9a64225b9750cf272ff46995
daiti0113 <stennis.ds.0113@gmail.com>
2020-09-22 23:43:13 +0900

    ESlintを追加 #4

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2020": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 11,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "linebreak-style": ["error","unix"],
+    "quotes": ["error", "double"],
+    "semi": ["error","never"],
+    "no-unused-vars": 2,
+    "react/jsx-uses-vars": 2,
+    "react/jsx-indent": ["error", 2],
+    "react/jsx-indent-props": ["error", 2],
+    "react/prop-types": [0],
+    "jsx-quotes": ["warn", "prefer-double"],
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "max-lines-per-function": ["error", 30],
+    "max-statements": ["error", 15],
+    "max-depth": ["error", 2],
+    "complexity": ["error", 5],
+    "no-delete-var": "error",
+    "no-var": "error",
+    "block-spacing": ["error", "never"],
+    "object-curly-spacing": ["error", "always"],
+    "comma-dangle": ["error", "never"]
+  }
+}
