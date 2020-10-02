93fc7b6dbc1dfe61b2e9881b5f37e233b388711b
daiti0113 <stennis.ds.0113@gmail.com>
2020-10-03 02:07:56 +0900

    APIモックをRESTからGraphQL(Apollo Server)に変更する #12

---
+++ mock/index.js
@@ -0,0 +1,46 @@
+// eslint-disable-next-line no-undef
+const {ApolloServer, gql} = require("apollo-server")
+
+// スキーマを定義する
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`
+
+// クエリで取得するデータを定数で置いておく
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crishton"
+  }
+]
+
+// クエリ発行時の処理を指定する
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+}
+
+const mocks = {
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => "Hello"
+}
+
+// サーバーを起動する
+const server = new ApolloServer({typeDefs, resolvers, mocks})
+
+server.listen().then(({url}) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
