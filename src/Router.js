517d6cb78add6d289b54424d6899d01f971f742c
daiti0113 <stennis.ds.0113@gmail.com>
2020-10-06 23:30:32 +0900

    検索画面を追加 #14

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from "@react-navigation/native"
 import {createStackNavigator} from "@react-navigation/stack"
 import {Home} from "./components/pages/Home"
 import {Login} from "./components/pages/Login"
+import {SearchPage} from "./components/pages/SearchPage"
 
 
 const Stack = createStackNavigator()
@@ -12,5 +13,6 @@ export const Router = () =>
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Search" component={SearchPage} />
     </Stack.Navigator>
   </NavigationContainer>
