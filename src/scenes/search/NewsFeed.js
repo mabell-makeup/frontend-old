3184780bc7bc4a2f9566be8f88ec3b074bf9e5fa
daiti0113 <stennis.ds.0113@gmail.com>
2020-10-08 00:27:11 +0900

    NewsFeedの叩き台を追加 #18

--- src/scenes/search/NewsFeed.js
+++ src/scenes/search/NewsFeed.js
@@ -1,11 +1,11 @@
 import React from "react"
 import {View, Text} from "react-native"
 import {StyleSheet} from "react-native"
-import {defaultStyle} from "../styles/defaultStyle"
+import {defaultStyle} from "../../styles/defaultStyle"
 
 const styles = StyleSheet.create(defaultStyle)
 
-export const Search = ({navigation}) => 
+export const NewsFeed = ({navigation}) => 
   <View style={styles.container}>
-    <Text>検索画面</Text>
+    <Text>NewsFeed</Text>
   </View>
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,14 @@
 import React from "react"
 import {createStackNavigator} from "@react-navigation/stack"
-import {Search} from "../scenes/Search"
+import {Search} from "../scenes/search/Search"
+import {NewsFeed} from "../scenes/search/NewsFeed"
 
 const Stack = createStackNavigator()
 
 const SearchStack = () => 
-  <Stack.Navigator initialRouteName="Search">
+  <Stack.Navigator initialRouteName="NewsFeed">
     <Stack.Screen name="Search" component={Search} />
+    <Stack.Screen name="NewsFeed" component={NewsFeed} />
   </Stack.Navigator>
 
 export const SearchScreen = () => <SearchStack />
