af6f0b8e359d4d447af278054b881bf1e1531ea6
daiti0113 <stennis.ds.0113@gmail.com>
2020-10-10 02:48:10 +0900

    BottomNavigationをcomponentsディレクトリに移動

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {createMaterialBottomTabNavigator} from "@react-navigation/material-bottom-tabs"
-import {HomeScreen} from "./screens/HomeScreen"
-import {SearchScreen} from "./screens/SearchScreen"
+import {HomeScreen} from "../screens/HomeScreen"
+import {SearchScreen} from "../screens/SearchScreen"
 import {NavigationContainer} from "@react-navigation/native"
 
 const Tab = createMaterialBottomTabNavigator()
@@ -11,9 +11,9 @@ export const BottomNavigation = () => {
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Home"
-        activeColor="#f0edf6"
-        inactiveColor="#3e2465"
-        barStyle={{backgroundColor: "#694fad"}}
+        activeColor="#fff"
+        inactiveColor="#777"
+        barStyle={{backgroundColor: "#333"}}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Search" component={SearchScreen} />
