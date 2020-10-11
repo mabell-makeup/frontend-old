d18162fd2785412a3c2d816396707ce3abe3172b
daiti0113 <stennis.ds.0113@gmail.com>
2020-10-11 16:06:42 +0900

    画面タップ時にキーボードが非表示になるように変更 #17

--- src/scenes/search/Search.js
+++ src/scenes/search/Search.js
@@ -1,12 +1,21 @@
 import React from "react"
-import {View, Text} from "react-native"
+import {View, Text, TouchableWithoutFeedback, Keyboard} from "react-native"
 import {StyleSheet} from "react-native"
 import {defaultStyle} from "../../styles/defaultStyle"
 
 const styles = StyleSheet.create(defaultStyle)
 
-export const Search = ({navigation}) => 
-  <View style={styles.container}>
-    <Text>検索画面</Text>
-  </View>
-  
+const DissmissKeyboard = ({children}) =>
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+
+export const Search = () => {
+  return (
+    <DissmissKeyboard>
+      <View style={styles.container}>
+        <Text>検索画面</Text>
+      </View>
+    </DissmissKeyboard>
+  )
+}
