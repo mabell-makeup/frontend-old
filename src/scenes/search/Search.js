bc8d1b230a1d2717c9ecee17d61919ea3473488a
daiti0113 <stennis.ds.0113@gmail.com>
2020-10-11 22:21:49 +0900

    DissmissKeyboardをApp全体をラップするように移動

--- src/scenes/search/Search.js
+++ src/scenes/search/Search.js
@@ -1,15 +1,8 @@
 import React from "react"
-import {TouchableWithoutFeedback, Keyboard} from "react-native"
 import {TopNavigation} from "../../components/TopNavigation"
 import {Men} from "./Men"
 import {Women} from "./Women"
 
-
-// TODO: DissmissKeyboardでApp全体をラップしたほうがいいのかも
-const DissmissKeyboard = ({children}) =>
-  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-    {children}
-  </TouchableWithoutFeedback>
 
 const screens = [
   {name: "全体", component: Women},
@@ -19,10 +12,4 @@ const screens = [
   {name: "ユーザー", component: Women}
 ]
 
-export const Search = () => {
-  return (
-    <DissmissKeyboard>
-      <TopNavigation screens={screens} />
-    </DissmissKeyboard>
-  )
-}
+export const Search = () => <TopNavigation screens={screens} />
