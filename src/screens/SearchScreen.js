52e4a3c5cb74198cd9f38318cd35732db2918ff7
daiti0113 <stennis.ds.0113@gmail.com>
2020-10-19 23:29:09 +0900

    searchStoreを追加 #31

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,24 +5,27 @@ import {Search} from "../scenes/search/Search"
 import {NewsFeed} from "../scenes/search/NewsFeed"
 import {SearchInput} from "../components/SearchInput"
 import {Text} from "react-native-paper"
+import {SearchProvider} from "../stores/searchStore"
 
 const Stack = createStackNavigator()
 
 export const SearchScreen = ({navigation}) => 
-  <Stack.Navigator
-    initialRouteName="NewsFeed"
-    screenOptions={{
-      headerStyle: {height: 60},
-      headerTitleStyle: {width: "70%"},
-      headerTitleAlign: "left",
-      headerBackTitleVisible: false
-    }}
-  >
-    {/* SearchbarのonChangeで再レンダリングされないようにheaderTitleにわたすコンポーネントは無名関数でラップする */}
-    <Stack.Screen name="Search" component={Search} options={{
-      headerTitle: () => <SearchInput isFocused={true} />,
-      headerRight: () => <Text onPress={() => navigation.reset({index: 0, routes: [{name: "NewsFeed"}]})}>キャンセル</Text>,
-      headerRightContainerStyle: {marginRight: 5}
-    }}/>
-    <Stack.Screen name="NewsFeed" component={NewsFeed} options={{headerTitle: () => <Text onPress={() => navigation.reset({index: 0, routes: [{name: "Search"}]})}>Search</Text>}}/>
-  </Stack.Navigator>
+  <SearchProvider>
+    <Stack.Navigator
+      initialRouteName="NewsFeed"
+      screenOptions={{
+        headerStyle: {height: 60},
+        headerTitleStyle: {width: "70%"},
+        headerTitleAlign: "left",
+        headerBackTitleVisible: false
+      }}
+    >
+      {/* SearchbarのonChangeで再レンダリングされないようにheaderTitleにわたすコンポーネントは無名関数でラップする */}
+      <Stack.Screen name="Search" component={Search} options={{
+        headerTitle: () => <SearchInput isFocused={true} />,
+        headerRight: () => <Text onPress={() => navigation.reset({index: 0, routes: [{name: "NewsFeed"}]})}>キャンセル</Text>,
+        headerRightContainerStyle: {marginRight: 5}
+      }}/>
+      <Stack.Screen name="NewsFeed" component={NewsFeed} options={{headerTitle: () => <Text onPress={() => navigation.reset({index: 0, routes: [{name: "Search"}]})}>Search</Text>}}/>
+    </Stack.Navigator>
+  </SearchProvider>
