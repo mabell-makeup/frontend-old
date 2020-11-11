f2a306a7fccdcf84212e78cf0385964ac9d1db39
daiti0113 <stennis.ds.0113@gmail.com>
2020-11-12 01:35:35 +0900

    PostにHeaderとImageを追加 #19

--- src/scenes/search/Post.js
+++ src/scenes/search/Post.js
@@ -1,6 +1,11 @@
 import React from "react"
-import {View, StyleSheet} from "react-native"
+import {View, StyleSheet, Image, Dimensions, ScrollView} from "react-native"
 import {Chip} from "react-native-paper"
+import {Appbar} from "react-native-paper"
+import {Platform} from "react-native"
+
+// TODO: Constantsに移動
+const ITEM_WIDTH = Dimensions.get("window").width
 
 const styles = StyleSheet.create({
   row: {
@@ -10,6 +15,11 @@ const styles = StyleSheet.create({
   },
   chip: {
     margin: 4
+  },
+  image: {
+    // TODO: もっといい書き方があるはず
+    width: ITEM_WIDTH,
+    height: 400
   }
 })
 
@@ -24,13 +34,25 @@ const tags = [
   "Push"
 ]
 
+const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical"
+
 export const Post = () => {
   return (
-    <View style={styles.row}>
-      {tags.map(tag => <Chip mode = "outlined" style={styles.chip}>
-        {tag}
-      </Chip>)}
-    </View>
+    <ScrollView>
+      <Appbar.Header>
+        <Appbar.Content title="Title" subtitle={"Subtitle"} />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+        <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+      </Appbar.Header>
+      <Image
+        source={{uri: "http://192.168.3.23:3000/img/posts/user3/1.jpg"}}
+        style={styles.image}
+      />
+      <View style={styles.row}>
+        {tags.map(tag => <Chip mode = "outlined" style={styles.chip}>
+          {tag}
+        </Chip>)}
+      </View>
+    </ScrollView>
   )
 } 
-
