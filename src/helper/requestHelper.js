a652227529f3acf6f2c95ffae9f712cd0b651ca1
daiti0113 <stennis.ds.0113@gmail.com>
2020-11-29 23:45:13 +0900

    Expoの環境変数を追加 #52

--- src/helper/requestHelper.js
+++ src/helper/requestHelper.js
@@ -1,10 +1,11 @@
 import {gql, useQuery} from "@apollo/client"
 import {mockRequest} from "../../mock"
+import Constants from "expo-constants"
 
 
 export const apiRequest = (queryLiterals="") => {
   // eslint-disable-next-line no-undef
-  if(process.env.NODE_ENV !== "production") return mockRequest(queryLiterals)
+  if(Constants.manifest.extra.env !== "production") return mockRequest(queryLiterals)
   const query = gql(queryLiterals)
   return useQuery(query)
 }
