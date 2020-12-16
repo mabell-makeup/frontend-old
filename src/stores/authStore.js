c82f3dfda35702258fec302629a67198b885383e
daiti0113 <stennis.ds.0113@gmail.com>
2020-12-17 00:44:17 +0900

    usernameとpasswordでのログインが可能になった #48

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,7 +1,6 @@
 import React, {createContext, useReducer} from "react"
-import {signup} from "../helper/authHelper"
-import {apiRequest} from "../helper/requestHelper"
 import {createReducer} from "../helper/storeHelper"
+import {Auth} from "aws-amplify"
 
 const initialState = {
   isLoggedIn: false
@@ -11,12 +10,48 @@ const initialState = {
 const authStore = createContext(initialState)
 
 // Define Types
-const LOGIN_SUCCESS = "LOGIN"
+const LOGIN_SUCCESS = "LOGIN_SUCCESS"
+const LOGOUT_SUCCESS = "LOGOUT_SUCCESS"
+const SIGNUP_SUCCESS = "SIGNUP_SUCCESS"
+
 
 // Define ActionCreator
-export const login = async (dispatch, username, password, email, nickname, gender, birthdate) => {
-  await signup(username, password, email, nickname, gender, birthdate)
+export const login = async (dispatch, username, password) => {
+  try {
+    const user = await Auth.signIn(username, password)
+    console.log(user)
+  } catch (error) {
+    console.log("error signing in", error)
+  }
   dispatch({type: LOGIN_SUCCESS, payload: true})
+}
+
+export const signup = async (dispatch, username, password, email, nickname, gender, birthdate) => {
+  try {
+    const {user} = await Auth.signUp({
+      username,
+      password,
+      attributes: {
+        email,
+        nickname,
+        gender,
+        birthdate
+      }
+    })
+    console.log("SIGNUP_USER:", user)
+  } catch (error) {
+    console.log("error signing up:", error)
+  }
+  dispatch({type: SIGNUP_SUCCESS, payload: true})
+}
+
+export const logout = async (dispatch) => {
+  try {
+    await Auth.signOut({global: true})
+  } catch (error) {
+    console.log("error signing out: ", error)
+  }
+  dispatch({type: LOGOUT_SUCCESS, payload: true})
 }
 
 
