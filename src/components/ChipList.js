c2dbde640b52126f5bd70eae79a7a55c049c989e
daiti0113 <stennis.ds.0113@gmail.com>
2021-01-24 13:09:55 +0900

    Chipを独自実装に変更

--- src/components/ChipList.js
+++ src/components/ChipList.js
@@ -1,8 +1,9 @@
 import React from "react"
-import {Chip} from "react-native-paper"
-import {View, StyleSheet} from "react-native"
+import {View, TouchableOpacity} from "react-native"
+import {Text, Checkbox} from "react-native-paper"
 
-const styles = StyleSheet.create({
+// eslint-disable-next-line max-lines-per-function
+const createStyles = (hasLeft, selected) => ({
   row: {
     flexDirection: "row",
     flexWrap: "wrap",
@@ -10,21 +11,67 @@ const styles = StyleSheet.create({
     marginVertical: 2
   },
   chip: {
-    margin: 4
+    margin: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingHorizontal: 6,
+    minWidth: 40,
+    height: 28,
+    borderWidth: 0.5,
+    borderRadius: 50,
+    borderColor: "#ccc",
+    backgroundColor: selected ? "#ddd" : "#fff"
   },
   chipText: {
     fontSize: 10,
     fontWeight: "bold",
-    marginLeft: 4,
-    marginRight: 4,
-    marginVertical: 0.5,
-    minWidth: 20,
-    textAlign: "center"
+    marginLeft: hasLeft || selected ? 25 : 0
+  },
+  leftContainer: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  leftItem: {
+    position: "absolute",
+    left: 0
+  },
+  checkbox: {
+    position: "absolute",
+    left: -8
   }
 })
 
 
-export const ChipList = ({items=[{label: ""}], style}) =>
-  <View style={{...styles.row, ...style}}>
-    {items.map(({label, ...props}) => <Chip key={label} mode="outlined" style={styles.chip} textStyle={styles.chipText} {...props}>{label}</Chip>)}
-  </View>
+const Chip = ({selected=false, left=false, children="", checkboxProps, ...props}) => {
+  const hasLeft = left !== false ? true : false // falseでないときはReact.Componentが入っている
+  const styles = createStyles(hasLeft, selected)
+
+  return (
+    <TouchableOpacity style={styles.chip} {...props}>
+      <View style={styles.leftContainer}>
+        {hasLeft && <View style={styles.leftItem}>{left}</View>}
+        {selected &&
+          <View style={styles.checkbox}>
+            <Checkbox
+              status={selected ? "checked" : "unchecked"}
+              color="#888"
+              {...checkboxProps}
+            />
+          </View>}
+      </View>
+      <Text style={styles.chipText}>{children}</Text>
+    </TouchableOpacity>
+  )
+}
+
+
+export const ChipList = ({items=[{label: ""}], style}) => {
+  const styles = createStyles()
+
+  return (
+    <View style={{...styles.row, ...style}}>
+      {items.map(({label, ...props}) => <Chip key={label} {...props}>{label}</Chip>)}
+    </View>
+  )
+}
