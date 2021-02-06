14bae048e811d6e5212e55d73cf855522ff844a5
daiti0113 <stennis.ds.0113@gmail.com>
2021-02-07 01:04:34 +0900

    文言修正 #89

--- src/scenes/login/RegisterUsername.js
+++ src/scenes/login/RegisterUsername.js
@@ -30,8 +30,8 @@ export const RegisterUsername = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-      <Title>ユーザー名を作成</Title>
-      <TextInput style={styles.input} mode="outlined" label="ユーザー名" onChangeText={text => updateNewUser(dispatch, {username: text})} />
+      <Title>表示名を作成</Title>
+      <TextInput style={styles.input} mode="outlined" label="表示名" onChangeText={text => updateNewUser(dispatch, {username: text})} />
       <Button style={styles.submit} mode="contained" onPress={() => navigation.navigate("RegisterPassword")}>次へ</Button>
     </View>
   )
