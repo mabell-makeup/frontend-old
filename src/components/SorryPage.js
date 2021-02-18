447771a028e73506e0726f37678b25b71a4b4b0d
daiti0113 <stennis.ds.0113@gmail.com>
2021-02-18 23:54:37 +0900

    ユーザー検索をSorryPageに変更

---
+++ src/components/SorryPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {Text, View} from "react-native"
+import {StyleSheet} from "react-native"
+import {defaultStyle} from "../styles/defaultStyle"
+
+const styles = StyleSheet.create(defaultStyle)
+
+export const SorryPage = () => 
+  <View style={styles.container}>
+    <Text>この機能は現在準備中です。</Text>
+  </View>
+
--- src/scenes/search/Search.js
+++ src/scenes/search/Search.js
@@ -1,20 +1,21 @@
 import React, {useContext} from "react"
 import {TopNavigation} from "../../components/TopNavigation"
 import {SelectConditions} from "./SelectConditions"
-import {searchStore, updateTmpConditions, fetchPosts} from "../../stores/searchStore"
+import {searchStore, updateTmpConditions} from "../../stores/searchStore"
+import {SorryPage} from "../../components/SorryPage"
 
 const screens = [
-  {label: "メイク", routeName: "SearchMakeup", key: "makeup"},
-  {label: "ユーザー", routeName: "SearchUser", key: "user"}
+  {label: "メイク", routeName: "SearchMakeup", key: "makeup", component: SelectConditions},
+  {label: "ユーザー", routeName: "SearchUser", key: "user", component: SorryPage}
 ]
 
 const createRows = (screens, dispatch, tmpConditions) => 
-  screens.map(screen => ({
-    label: screen.label,
-    routeName: screen.routeName,
-    key: screen.key,
-    component: SelectConditions,
-    listeners: {tabPress: () => updateTmpConditions(dispatch, tmpConditions, {target: screen.key})}
+  screens.map(({label, routeName, key, component}) => ({
+    label,
+    routeName,
+    key,
+    component,
+    listeners: {tabPress: () => updateTmpConditions(dispatch, tmpConditions, {target: key})}
   }))
 
 export const Search = () => {
