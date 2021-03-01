9960658d58f192510bd2e1e6f7f53e3454583301
daiti0113 <stennis.ds.0113@gmail.com>
2021-03-01 19:20:15 +0900

    ImageListにLoadingを追加 #145

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,6 @@
-import React from "react"
-import {FlatList, StyleSheet, Image, TouchableHighlight} from "react-native"
+import React, {useState} from "react"
+import {FlatList, StyleSheet, Image, TouchableHighlight, View} from "react-native"
+import {ActivityIndicator} from "react-native-paper"
 import {WINDOW_WIDTH} from "../styles/constants"
 
 const styles = StyleSheet.create({
@@ -8,23 +9,40 @@ const styles = StyleSheet.create({
     height: WINDOW_WIDTH / 3,
     margin: 1,
     resizeMode: "cover"
+  },
+  spinner: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
   }
 })
 
-const renderItem = ({item}) =>
-  <TouchableHighlight onPress={item.onPress}>
-    <Image
-      // eslint-disable-next-line no-undef
-      source={item.imgSrc !== "" ? {uri: item.imgSrc} : require("../../assets/no_image.png")}
-      style={styles.imageStyle}
-    />
-  </TouchableHighlight>
+const Item = ({item}) => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  return (
+    <TouchableHighlight onPress={item.onPress}>
+      <View>
+        <Image
+          // eslint-disable-next-line no-undef
+          source={item.imgSrc !== "" ? {uri: item.imgSrc} : require("../../assets/no_image.png")}
+          style={styles.imageStyle}
+          onLoadStart={() => setIsLoading(true)}
+          onLoadEnd={() => setIsLoading(false)}
+        />
+        <ActivityIndicator animating={isLoading} color="tomato" style={styles.spinner} />
+      </View>
+    </TouchableHighlight>
+  )
+}
 
 export const ImageList = ({data, style, onEndReached=()=>{}, scrollEnabled=true}) => {
   return (
     <FlatList
       data={data}
-      renderItem={renderItem}
+      renderItem={({item}) => <Item item={item} />}
       keyExtractor={(item, idx) => item.id + idx}
       numColumns={3}
       style={style}
