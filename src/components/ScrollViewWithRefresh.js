42019976788f2366b56b1808a8088f2aae1b3eed
daiti0113 <stennis.ds.0113@gmail.com>
2021-04-11 01:37:57 +0900

    ローディングの色を変更 & 最短でも1秒は表示するように修正 #179

--- src/components/ScrollViewWithRefresh.js
+++ src/components/ScrollViewWithRefresh.js
@@ -1,8 +1,9 @@
 import React, {useState} from "react"
 import {RefreshControl, ScrollView} from "react-native"
+import {primary} from "../styles/colors"
 
 
-export const ScrollViewWithRefresh = ({refreshFunc, children}) => {
+export const ScrollViewWithRefresh = ({refreshFunc, children, ...props}) => {
   const [isRefreshing, setIsRefreshing] = useState(false)
 
   return (
@@ -10,13 +11,18 @@ export const ScrollViewWithRefresh = ({refreshFunc, children}) => {
       refreshControl={
         <RefreshControl
           refreshing={isRefreshing}
+          colors={[primary]}
+          tintColor={primary}
           onRefresh={async () => {
             setIsRefreshing(true)
-            await refreshFunc()
-            setIsRefreshing(false)
+            const promise = refreshFunc()
+            setTimeout(() => {
+              promise.then(() => setIsRefreshing(false))
+            }, 1000) // 最短でも1秒間はローディングを表示する
           }}
         />
       }
+      {...props}
     >
       {children}
     </ScrollView>
